198ec25d43f931e65609a86bbff6e6ab580abd47
Kemal Ozturk <51798197+ozturkkl@users.noreply.github.com>
2021-11-27 18:34:05 -0500

    Created nav component

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,26 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './components/app/app.component';
 import { CanvasComponent } from './components/canvas/canvas.component';
 import { NavComponent } from './components/nav/nav.component';
-import { ButtonComponent } from './components/button/button.component';
 import { ImageListComponent } from './components/image-list/image-list.component';
 import { ImageItemComponent } from './components/image-item/image-item.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     CanvasComponent,
     NavComponent,
-    ButtonComponent,
     ImageListComponent,
     ImageItemComponent,
     WelcomeComponent,
   ],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
