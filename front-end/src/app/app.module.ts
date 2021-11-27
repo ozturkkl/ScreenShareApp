aa5ab890c296c604ba577db48bfc86e84d33123b
Kemal Ozturk <51798197+ozturkkl@users.noreply.github.com>
2021-11-27 17:10:55 -0500

    Initial commit & Front end components generated.

---
+++ front-end/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './components/app/app.component';
+import { CanvasComponent } from './components/canvas/canvas.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ButtonComponent } from './components/button/button.component';
+import { ImageListComponent } from './components/image-list/image-list.component';
+import { ImageItemComponent } from './components/image-item/image-item.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CanvasComponent,
+    NavComponent,
+    ButtonComponent,
+    ImageListComponent,
+    ImageItemComponent,
+    WelcomeComponent,
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
